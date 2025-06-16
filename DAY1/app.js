86a3b062339da41e7c535da1f19f0de506398dd1
venkatesh <venkateshkondapall18k@gmail.com>
2025-06-16 13:27:49 +0530

    day 1

---
+++ DAY1/app.js
@@ -0,0 +1,55 @@
+ window.console.log("hello From js! .. ");
+
+ //let const var
+
+//  let age=30;
+//  const year= 2020;
+//  var block =25;
+//  console.log(age, year, block);
+// output=(30, 2020, 25)
+// //   ------------------
+// console.log(age);
+// var age = 20;   // declaration & intitilization 
+
+// console.log(age);
+
+// var age=30;  // re -declaration
+// console.log(age);
+// output undefined   20   30
+//-------------------------------
+
+
+// let age=22;
+// console.log(age);
+// age =23;
+// console.log(age);
+
+// console.log(1+1);
+// console.log('1'+1);
+// console.log(1+'1');
+// console.log('1'+'1');
+
+
+// console.log(1-1);
+// console.log('a'-1);
+// console.log(1-'a');
+// console.log('1'-'a');
+
+
+// console.log(1*1);
+// console.log('a'*1);
+// console.log(1*'a');
+// console.log('1'*'a');
+
+
+
+// console.log(1/1);
+// console.log('a'/1);
+// console.log(1/'a');
+// console.log('1'/'a');
+// in javascript it will convert the string to a number and appliy arthemtic opertions is called type coersion implict 
+console.log(1-1);
+console.log('5'-1);
+console.log(1-'3');
+console.log('6'-'7');
+
